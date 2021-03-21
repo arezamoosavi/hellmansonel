8b25c6bdc0ccb880ca659a1e2f24ca4bc558f487
alireza <sdamoosavi@gmail.com>
2021-03-21 18:04:25 +0330

    dags and docker img modified

---
+++ dags/dag_spark_run_pca.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+args = {"owner": "airflow", "provide_context": True}
+
+dag = DAG(
+    dag_id="spark_run_pca",
+    schedule_interval=None,
+    start_date=datetime(year=2021, month=2, day=20, hour=4, minute=0, second=0),
+    concurrency=4,
+    max_active_runs=1,
+    default_args=args,
+)
+
+start_task = BashOperator(
+    task_id="start_task", bash_command="echo spark_run_pca", dag=dag,
+)
+
+spark_run_pca = BashOperator(
+    task_id="run_spark_validation_check_core_local",
+    bash_command="""spark-submit --verbose --master local dags/app_services/spark_pca.jar""",
+    dag=dag,
+)
+
+start_task >> spark_run_pca
--- dockers/airflow/run.sh
+++ dockers/airflow/run.sh
@@ -4,6 +4,22 @@
 set -o errexit
 set -o nounset
 
+start-history-server.sh
+echo "History server is sarting ...."
+sleep 2
+echo "Master is sarting ...."
+start-master.sh
+
+sleep 1
+echo "Master started at port 8080 ...."
+
+echo "worker is sarting ...."
+
+start-worker.sh spark://airflow:7077
+
+sleep 1
+echo "worker started at port 8081 ...."
+
 airflow initdb
 
 sleep 2
